8a2fc15602b7be65ec1adf8e5b87645b4e6e184c
Kurukshetra <kurukshetracodingschool@gmail.com>
2025-01-15 20:58:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+const time =  document.querySelector(".time");
+const date =  document.querySelector(".date");
+
+const days = ["Sunday","Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "Jun", "July", "August", "September", "October", "November", "December"];
+
+function steTime(){
+    
+}
+
+// hour.style.transform =`translate(-50% , -100%) rotate(${scale(hourForClock,0,12,0,360)}deg)`;
